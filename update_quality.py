a034c23ae6b6f984d363543a07985463827aba61
Ryan Whitley <rwhitley45@gmail.com>
2021-08-22 11:56:39 -0500

    create quality updater object, init solution

--- update_quality.py
+++ update_quality.py
@@ -1,32 +1,52 @@
 def update_quality(awards):
+    updater = QualityUpdater()
     for award in awards:
-        if award.name != 'Blue First' and award.name != 'Blue Compare':
+        if award.name == "Blue First":
+            updater.update_blue_first(award)
+        elif award.name == "Blue Compare":
+            updater.update_blue_compare(award)
+        elif award.name == "Blue Star":
+            updater.update_blue_star(award)
+        elif award.name.startswith("NORMAL"):
+            updater.update_normal(award)
+
+
+class QualityUpdater:
+    def update_blue_star(self, award):
+        if award.quality > 0:
+            award.quality -= 2
+        award.expires_in -= 1
+        if award.expires_in < 0:
             if award.quality > 0:
-                if award.name != 'Blue Distinction Plus':
-                    award.quality -= 1
-        else:
+                award.quality -= 2
+        if award.quality < 0:
+            award.quality = 0
+
+    def update_blue_first(self, award):
+        if award.quality < 50:
+            award.quality += 1           
+        award.expires_in -= 1
+        if award.expires_in < 0:
             if award.quality < 50:
                 award.quality += 1
-                if award.name == 'Blue Compare':
-                    if award.expires_in < 11:
-                        if award.quality < 50:
-                            award.quality += 1
-                    if award.expires_in < 6:
-                        if award.quality < 50:
-                            award.quality += 1
 
-        if award.name != 'Blue Distinction Plus':
-            award.expires_in -= 1
-
+    def update_blue_compare(self, award):
+        if award.quality < 50:
+            award.quality += 1
+        if award.expires_in < 11:
+            if award.quality < 50:
+                award.quality += 1
+        if award.expires_in < 6:
+            if award.quality < 50:
+                award.quality += 1               
+        award.expires_in -= 1
         if award.expires_in < 0:
-            if award.name != 'Blue First':
-                if award.name != 'Blue Compare':
-                    if award.quality > 0:
-                        if award.name != 'Blue Distinction Plus':
-                            award.quality -= 1
-                else:
-                    award.quality = award.quality - award.quality
-            else:
-                if award.quality < 50:
-                    award.quality += 1
+            award.quality = award.quality - award.quality
 
+    def update_normal(self, award):
+        if award.quality > 0:
+            award.quality -= 1
+        award.expires_in -= 1      
+        if award.expires_in < 0:
+            if award.quality > 0:
+                award.quality -= 1
